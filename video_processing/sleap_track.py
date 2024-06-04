31942fd9f90ee751388c72127855e2d4ba123f06
krgykzt <krgykztzy@gmail.com>
2024-06-04 15:17:28 -0700

    .

--- video_processing/sleap_track.py
+++ video_processing/sleap_track.py
@@ -12,33 +12,36 @@ def sleap_track(video_folder, model_path):
     '''
     
     # Logger setup
-    logger_location = str(video_folder) + '/logger'
+    logger_location = os.path.join(video_folder, 'logger')
     if not os.path.isdir(logger_location):
         os.makedirs(logger_location)
-    logger = setup_logger(str(video_folder).split('/')[-1], logger_location)
+    logger = setup_logger(os.path.basename(video_folder), logger_location)
     
-    # Track videos
-    track_videos(video_folder, model_path, logger)
-
-    # Convert SLEAP files
-    convert_slp_files(video_folder, logger)
-
-    
-    for filename in os.listdir(video_folder):
-        if filename.endswith('analysis.h5'):
+    # Walk through all subdirectories in the video_folder
+    for root, dirs, files in os.walk(video_folder):
+        for subdir in dirs:
+            subdir_path = os.path.join(root, subdir)
             
-            input_h5 = os.path.join(video_folder, filename)
-            # Warp all coordinates to align the videos from different cameras
-            df = transform_coordinates(input_h5, logger)
-            
-            appended_df = append_time_info(df, video_folder, filename, logger)
+            # Track videos
+            track_videos(subdir_path, model_path, logger)
 
-            # Assign the output path
-            parts = filename.split('_')
-            base = '_'.join(parts[:3])
-            output_path = os.path.join(video_folder, base + '_tracks_raw.csv')
-            
-            appended_df.to_csv(output_path)
-            
-            logger.info(f'Preprocess done for: {str(filename).split(".")[0]}')
+            # Convert SLEAP files
+            convert_slp_files(subdir_path, logger)
 
+            for filename in os.listdir(subdir_path):
+                if filename.endswith('analysis.h5'):
+                    input_h5 = os.path.join(subdir_path, filename)
+                    
+                    # Warp all coordinates to align the videos from different cameras
+                    df = transform_coordinates(input_h5, logger)
+                    
+                    appended_df = append_time_info(df, subdir_path, filename, logger)
+
+                    # Assign the output path
+                    parts = filename.split('_')
+                    base = '_'.join(parts[:3])
+                    output_path = os.path.join(subdir_path, base + '_tracks_raw.csv')
+                    
+                    appended_df.to_csv(output_path)
+                    
+                    logger.info(f'Preprocess done for: {filename}')
